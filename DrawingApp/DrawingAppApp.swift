e3b5513099567dea64df801a1baa194b85440a03
Kayla <kdizayer1@gmail.com>
2024-02-08 22:36:55 -0800

    Initial Commit

---
+++ DrawingApp/DrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAppApp.swift
+//  DrawingApp
+//
+//  Created by Kayla Dizayer on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
